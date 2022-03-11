2afbed9bf57dfb43047c274a886a4e26f939a1a8
ykrantz <ykrantz@gmail.com>
2022-03-11 17:05:30 +0200

    adding MUI

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,7 +1,7 @@
 import "./MainPage.css";
 import Projects from "./Projects/Projects";
 import playListLogo from "../../images/welcome_page_known_user_b.PNG";
-import globalShopLogo from "../../images/shops-g0fd51ba89_640.jpg";
+import onlineShopLogo from "../../images/online-shop.PNG";
 import panchTimeLogo from "../../images/clock-gb966335cb_640.jpg";
 import ticTacToeLogo from "../../images/tic-tac-toe.PNG";
 import groceryShopLogo from "../../images/grocery-gf851f3910_640.jpg";
@@ -9,39 +9,45 @@ import groceryShopLogo from "../../images/grocery-gf851f3910_640.jpg";
 const MainPage = () => {
   const myProjects = [
     {
-      title: "React + Node.js + MongoDB",
+      frameWork: "React + Node.js + MongoDB",
       projectName: "Video Play List",
       image: playListLogo,
       url: "https://playlist-ykrantz.netlify.app/",
+      avatar: "VP",
     },
+
     {
-      title: "React ",
-      projectName: "Shop",
-      image: globalShopLogo,
-      url: "",
-    },
-    {
-      title: "React + Node.js + MongoDB",
+      frameWork: "React + Node.js + MongoDB",
       projectName: "Panck Time Watch",
       image: panchTimeLogo,
       url: "",
+      avatar: "PT",
     },
     {
-      title: "HTML (vanila)",
+      frameWork: "React ",
+      projectName: "Online Shop",
+      image: onlineShopLogo,
+      url: "https://online-shop-ykrantz.netlify.app/",
+      avatar: "OS",
+    },
+    {
+      frameWork: "HTML (vanila)",
       projectName: "Tic Tac Toe",
       image: ticTacToeLogo,
       url: "https://tic-tac-toe-ykrantz.netlify.app/",
+      avatar: "TT",
     },
     {
-      title: "HTML (vanila)",
+      frameWork: "HTML (vanila)",
       projectName: "Grocery Store",
       image: groceryShopLogo,
       url: "",
+      avatar: "GS",
     },
   ];
   return (
     <div>
-      <h1>main page</h1>
+      <h1 className="MainPage-title">My Portfolio</h1>
       <div className="MainPage-projectsContainer">
         <Projects myProjects={myProjects} />
       </div>
