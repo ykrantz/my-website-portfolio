7a53e02af86aabfa222b9d0980a405f111acd34d
ykrantz <ykrantz@gmail.com>
2022-03-13 00:54:56 +0200

    adding chuck norris and arrange sort

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -5,44 +5,82 @@ import onlineShopLogo from "../../images/online-shop.PNG";
 import panchTimeLogo from "../../images/clock-gb966335cb_640.jpg";
 import ticTacToeLogo from "../../images/tic-tac-toe.PNG";
 import groceryShopLogo from "../../images/grocery-gf851f3910_640.jpg";
+import chuckNorris from "../../images/chuckNorris.PNG";
 
 const MainPage = () => {
   const myProjects = [
     {
+      id: 10,
       frameWork: "React + Node.js + MongoDB",
       projectName: "Video Play List",
       image: playListLogo,
       url: "https://playlist-ykrantz.netlify.app/",
       avatar: "VP",
+      active: true,
     },
 
     {
+      id: 20,
       frameWork: "React + Node.js + MongoDB",
       projectName: "Panck Time Watch",
       image: panchTimeLogo,
       url: "",
       avatar: "PT",
+      active: false,
     },
     {
-      frameWork: "React ",
+      id: 12,
+      frameWork: "React + API ",
       projectName: "Online Shop",
       image: onlineShopLogo,
       url: "https://online-shop-ykrantz.netlify.app/",
       avatar: "OS",
+      active: true,
     },
     {
-      frameWork: "HTML (vanila)",
+      id: 14,
+      frameWork: "HTML (vanila)+ JS",
       projectName: "Tic Tac Toe",
       image: ticTacToeLogo,
       url: "https://tic-tac-toe-ykrantz.netlify.app/",
       avatar: "TT",
+      active: true,
     },
     {
-      frameWork: "HTML (vanila)",
+      id: 50,
+      frameWork: "HTML (vanila) + JS",
       projectName: "Grocery Store",
       image: groceryShopLogo,
       url: "",
       avatar: "GS",
+      active: false,
+    },
+    {
+      id: 60,
+      frameWork: "React",
+      projectName: "TO DO LIST",
+      image: groceryShopLogo,
+      url: "",
+      avatar: "TD",
+      active: false,
+    },
+    {
+      id: 70,
+      frameWork: "React + Redux",
+      projectName: "TO DO LIST- REDUX",
+      image: groceryShopLogo,
+      url: "",
+      avatar: "TD",
+      active: false,
+    },
+    {
+      id: 16,
+      frameWork: "HTML (vanila) + JS + API",
+      projectName: "Chuck Noris Jokes",
+      image: chuckNorris,
+      url: "https://chuck-norris-ykrantz.netlify.app/",
+      avatar: "CN",
+      active: true,
     },
   ];
   return (
--- src/components/MainPage/Projects/Projects.jsx
+++ src/components/MainPage/Projects/Projects.jsx
@@ -5,9 +5,10 @@ import Project from "../Project/Project";
 
 const Projects = ({ myProjects }) => {
   console.log(myProjects);
+  myProjects.sort((a, b) => a.id - b.id);
   return (
     <div>
-      <h1>Projects</h1>
+      <h1>Projects :</h1>
       <div className="Projects-projectContainer">
         {myProjects.map((project) => (
           <Project key={project.projectName} project={project} />
