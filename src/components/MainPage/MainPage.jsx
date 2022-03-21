66089850ba21deac52b0771e5605876b90607be9
ykrantz <ykrantz@gmail.com>
2022-03-21 21:56:44 +0200

    update details

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,6 +1,6 @@
 import "./MainPage.css";
 import Projects from "./Projects/Projects";
-import playListLogo from "../../images/welcome_page_known_user_b.PNG";
+import playListLogo from "../../images/welcome_page_known_user_c.png";
 import onlineShopLogo from "../../images/online-shop.PNG";
 import panchTimeLogo from "../../images/panckTimeClock.png";
 import ticTacToeLogo from "../../images/tic-tac-toe.PNG";
@@ -26,9 +26,8 @@ const MainPage = () => {
       active: true,
       details: `JWT athorizaiotn || Password bcrypt
       AJAX ||
-      DB updated each tranzaction ||
-      React hooks||
-      YouTube API
+      React hooks ||
+      DB updated each tranzaction 
       `,
 
       features: [
@@ -43,6 +42,57 @@ const MainPage = () => {
       ],
     },
 
+    {
+      id: 12,
+      frameWork: "HTML (vanila) + CSS + JS",
+      projectName: "Tic Tac Toe",
+      image: ticTacToeLogo,
+      url: "https://tic-tac-toe-ykrantz.netlify.app/",
+      gitHubUrlClient: "https://github.com/ykrantz/Tic_Tac_Toe",
+      gitHubUrlServer: "",
+      avatar: "TT",
+      active: true,
+      details: "HTML || CSS || HTML Query Selectors || JavaScript",
+      features: [
+        "Play with your friend Tic Tac Toe until winning",
+        "Decide how match row/columns you wnt in game between 3-10 row/columns",
+        "Go back moves if you regret, and continue playing from where you want to continue",
+        "Show the highest score (the minimun torns to win) and reset the highest score",
+        "Save game in your loacl storage and load the game",
+        "Reset Game after finish the game",
+        "Chabnge playes name if you wnat",
+      ],
+    },
+    {
+      id: 13,
+      frameWork: "React + API ",
+      projectName: "Online Shop",
+      image: onlineShopLogo,
+      url: "https://online-shop-ykrantz.netlify.app/",
+      gitHubUrlClient: "https://github.com/ykrantz/online-shop",
+      gitHubUrlServer: "",
+      avatar: "OS",
+      active: true,
+      details: "React || AJAX || React hooks ",
+      features: [
+        "- Filter products and sort according to your preferance",
+        "- Add products to cart and get total price",
+      ],
+    },
+    {
+      id: 15,
+      frameWork: "HTML (vanila) + JS + API",
+      projectName: "Memory Game",
+
+      image: memoryGame,
+      url: "https://memory-game-ykrantz.netlify.app/",
+      gitHubUrlClient: "https://github.com/ykrantz/memory-game",
+      gitHubUrlServer: "",
+      avatar: "MG",
+      active: true,
+      details: "HTML || CSS || HTML Query Selectors || JavaScript",
+      features: ["- Guess cards that match"],
+    },
     {
       id: 20,
       frameWork: "React + Node.js + MongoDB",
@@ -62,57 +112,6 @@ const MainPage = () => {
         "- CalcUlate total hours of the last shift",
         "- Get details of all shift that a worker did",
       ],
-    },
-    {
-      id: 16,
-      frameWork: "React + API ",
-      projectName: "Online Shop",
-      image: onlineShopLogo,
-      url: "https://online-shop-ykrantz.netlify.app/",
-      gitHubUrlClient: "https://github.com/ykrantz/online-shop",
-      gitHubUrlServer: "",
-      avatar: "OS",
-      active: true,
-      details: "React || AJAX || React hooks ",
-      features: [
-        "- Filter products and sort according to your preferance",
-        "- Add products to cart and get total price",
-      ],
-    },
-    {
-      id: 14,
-      frameWork: "HTML (vanila) + CSS + JS",
-      projectName: "Tic Tac Toe",
-      image: ticTacToeLogo,
-      url: "https://tic-tac-toe-ykrantz.netlify.app/",
-      gitHubUrlClient: "https://github.com/ykrantz/Tic_Tac_Toe",
-      gitHubUrlServer: "",
-      avatar: "TT",
-      active: true,
-      details: "HTML || JavaScript || HTML Query Selectors || CSS ",
-      features: [
-        "Play with your friend Tic Tac Toe until winning",
-        "Decide how match row/columns you wnt in game between 3-10 row/columns",
-        "Go back moves if you regret, and continue playing from where you want to continue",
-        "Show the highest score (the minimun torns to win) and reset the highest score",
-        "Save game in your loacl storage and load the game",
-        "Reset Game after finish the game",
-        "Chabnge playes name if you wnat",
-      ],
-    },
-    {
-      id: 15,
-      frameWork: "HTML (vanila) + JS + API",
-      projectName: "Memory Game",
-
-      image: memoryGame,
-      url: "https://memory-game-ykrantz.netlify.app/",
-      gitHubUrlClient: "https://github.com/ykrantz/memory-game",
-      gitHubUrlServer: "",
-      avatar: "MG",
-      active: true,
-      details: " HTML || JavaScript || HTML Query Selectors || CSS",
-      features: ["- Guess cards that match"],
     },
     {
       id: 22,
